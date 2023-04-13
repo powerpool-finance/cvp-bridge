3dae8cb988c7d23d60c89e0bb7877b72a56a32d5
defi-dev <defi.dev@protonmail.com>
2023-04-14 01:23:46 +0800

    fix lack of documentation

--- contracts/CvpBridgeLocker.sol
+++ contracts/CvpBridgeLocker.sol
@@ -28,6 +28,13 @@ contract CvpBridgeLocker is Ownable {
     currentChainId = _currentChainId;
   }
 
+  /**
+    @title Send CVP Tokens to another blockchain
+    @dev Sends CVP tokens to the specified recipient on the specified chain.
+    @param _toChainID ID of the destination chain.
+    @param _amount Amount of CVP tokens to be sent.
+    @param _recipient Address of the recipient on the destination chain.
+  */
   function sendToChain(uint256 _toChainID, uint256 _amount, address _recipient) external payable {
     require(destinationChainContracts[_toChainID] != address(0), "Chain contract address not specified");
     _checkChainLimits(_toChainID, _amount);
@@ -38,6 +45,13 @@ contract CvpBridgeLocker is Ownable {
     _send(dstTxCall, _toChainID, 0);
   }
 
+  /**
+    @title Unlock CVP Tokens received from another blockchain
+    @dev Unlocks CVP tokens received from the specified chain and sends them to the specified recipient.
+    @param _fromChainID ID of the source chain.
+    @param _amount Amount of CVP tokens to be unlocked.
+    @param _recipient Address of the recipient of the unlocked CVP tokens.
+  */
   function unlock(uint256 _fromChainID, uint256 _amount, address _recipient) external {
     require(sourceChainsContracts[_fromChainID] != address(0), "Chain contract address not specified");
     _onlyCrossChain(_fromChainID);
@@ -46,6 +60,12 @@ contract CvpBridgeLocker is Ownable {
     cvp.safeTransfer(_recipient, _amount);
   }
 
+  /**
+    @title Check chain limits for cross-chain transfers
+    @dev Checks if the specified amount of tokens can be transferred to the specified chain based on the daily limit set for the chain.
+    @param _chainID ID of the chain being checked for the transfer limit.
+    @param _amount Amount of tokens to be transferred.
+  */
   function _checkChainLimits(uint256 _chainID, uint256 _amount) internal {
     uint256 curEpoch = block.timestamp / 7 days;
     transfersPerDay[_chainID][curEpoch] += _amount;
@@ -53,6 +73,11 @@ contract CvpBridgeLocker is Ownable {
     require(chainLimitPerDay[_chainID] >= transfersPerDay[_chainID][curEpoch], "Limit reached");
   }
 
+  /**
+    @title Check if function is called by a valid cross-chain contract
+    @dev Checks if the function is being called by a valid cross-chain contract for the specified source chain.
+    @param _fromChainID ID of the source chain.
+  */
   function _onlyCrossChain(uint256 _fromChainID) internal {
     ICallProxy callProxy = ICallProxy(deBridgeGate.callProxy());
 
@@ -65,20 +90,34 @@ contract CvpBridgeLocker is Ownable {
     require(keccak256(abi.encodePacked(sourceChainsContracts[chainIdFrom])) == keccak256(nativeSender), "Not valid sender");
   }
 
+  /**
+    @title Encode the unlock command for cross-chain transfer
+    @dev Encodes the unlock command with the specified amount and recipient address for the current chain ID.
+    @param _amount Amount of tokens to be unlocked.
+    @param _recipient Address of the recipient of the unlocked tokens.
+    @return The encoded unlock command.
+  */
   function _encodeUnlockCommand(uint256 _amount, address _recipient)
     internal
     view
     returns (bytes memory)
   {
     return
-      abi.encodeWithSelector(
-        CvpBridgeLocker.unlock.selector,
-        currentChainId,
-        _amount,
-        _recipient
-      );
+    abi.encodeWithSelector(
+      CvpBridgeLocker.unlock.selector,
+      currentChainId,
+      _amount,
+      _recipient
+    );
   }
 
+  /**
+    @title Send the transaction to the specified chain
+    @dev Sends the transaction to the specified chain using the deBridgeGate.
+    @param _dstTransactionCall The encoded transaction to be sent to the destination chain.
+    @param _toChainId The ID of the destination chain.
+    @param _executionFee The execution fee to be paid to the executor.
+  */
   function _send(bytes memory _dstTransactionCall, uint256 _toChainId, uint256 _executionFee) internal {
     //
     // sanity checks
@@ -123,7 +162,7 @@ contract CvpBridgeLocker is Ownable {
     autoParams.data = _dstTransactionCall;
     autoParams.fallbackAddress = abi.encodePacked(msg.sender);
 
-    deBridgeGate.send{value: msg.value}(
+    deBridgeGate.send{value : msg.value}(
       address(0), // _tokenAddress
       amountToBridge, // _amount
       _toChainId, // _chainIdTo
@@ -135,22 +174,47 @@ contract CvpBridgeLocker is Ownable {
     );
   }
 
+  /**
+    @title Allows the owner to set the address of the DeBridgeGate contract.
+    @param _deBridgeGate Address of the new DeBridgeGate contract.
+  */
   function setDeBridgeGate(IDeBridgeGate _deBridgeGate) external onlyOwner {
     deBridgeGate = _deBridgeGate;
   }
 
+  /**
+    @dev Allows the owner to set the address of the destination chain contract for a given chain ID.
+    @param _chainId uint256 ID of the chain to set the destination chain contract for.
+    @param _contract address Address of the destination chain contract to set.
+  */
   function setDestinationChainContract(uint256 _chainId, address _contract) external onlyOwner {
     destinationChainContracts[_chainId] = _contract;
   }
 
+  /**
+    @dev Allows the owner to set the address of the source chain contract for a given chain ID.
+    @param _chainId uint256 ID of the chain to set the source chain contract for.
+    @param _contract address Address of the source chain contract to set.
+  */
   function setSourceChainContract(uint256 _chainId, address _contract) external onlyOwner {
     sourceChainsContracts[_chainId] = _contract;
   }
 
+  /**
+    @dev Allows the owner to set the maximum transfer limit per day for a specific chain.
+    @param _chainId uint256 ID of the chain for which the limit needs to be set.
+    @param _amount uint256 Maximum amount that can be transferred per day.
+  */
   function setChainLimitPerDay(uint256 _chainId, uint256 _amount) external onlyOwner {
     chainLimitPerDay[_chainId] = _amount;
   }
 
+  /**
+    @dev Transfers _amount tokens of _token to the _newBridge address for migration purposes.
+    @param _token address of the token to be migrated.
+    @param _newBridge address of the new bridge where the tokens will be migrated.
+    @param _amount amount of tokens to be migrated.
+  */
   function migrate(address _token, address _newBridge, uint256 _amount) external onlyOwner {
     IERC20(_token).safeTransfer(_newBridge, _amount);
   }
